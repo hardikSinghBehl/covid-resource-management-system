e7c233adb98c2442191538a8f74e230caec2bfbf
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-09 17:12:08 +0530

    initial project commit

---
+++ src/main/java/com/hardik/pomfrey/CovidResourceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.pomfrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidResourceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidResourceManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardik/pomfrey/CovidResourceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardik.pomfrey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidResourceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
