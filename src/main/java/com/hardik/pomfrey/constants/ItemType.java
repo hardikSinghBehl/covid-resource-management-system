a9a9aa11e5382a5aed94bb2a902f564db5430bc3
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-10 08:15:17 +0530

    added constants

---
+++ src/main/java/com/hardik/pomfrey/constants/ItemType.java
@@ -0,0 +1,12 @@
+package com.hardik.pomfrey.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ItemType {
+
+	public final String REQUEST = "request";
+	public final String RESPONSE = "response";
+	public final String COMMENT = "comment";
+
+}
---
+++ src/main/java/com/hardik/pomfrey/constants/State.java
@@ -0,0 +1,11 @@
+package com.hardik.pomfrey.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class State {
+
+	public final Boolean ACTIVE = true;
+	public final Boolean INACTIVE = false;
+
+}
