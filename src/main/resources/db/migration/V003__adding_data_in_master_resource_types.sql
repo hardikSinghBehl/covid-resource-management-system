a98111c488eb31675259edd85f270680e2c5e641
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-10 08:27:44 +0530

    added migration script for adding data to master_resource_type

---
+++ src/main/resources/db/migration/V003__adding_data_in_master_resource_types.sql
@@ -0,0 +1,8 @@
+INSERT INTO master_resource_types (name) VALUES ("Oxygen");
+INSERT INTO master_resource_types (name) VALUES ("Plasma");
+INSERT INTO master_resource_types (name) VALUES ("Hospital Beds");
+INSERT INTO master_resource_types (name) VALUES ("Quarantine Rooms");
+INSERT INTO master_resource_types (name) VALUES ("Food Delivery");
+INSERT INTO master_resource_types (name) VALUES ("Fund Raiser");
+INSERT INTO master_resource_types (name) VALUES ("Medicince");
+INSERT INTO master_resource_types (name) VALUES ("Other");
