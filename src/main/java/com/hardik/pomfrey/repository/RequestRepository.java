4928f2c432d893af9e4cbd773b37237f627c1660
Hardik Singh Behl <hardik.behl7444@gmail.com>
2021-05-11 14:18:08 +0530

    added native sql query to get nearest locations in repositories

--- src/main/java/com/hardik/pomfrey/repository/RequestRepository.java
+++ src/main/java/com/hardik/pomfrey/repository/RequestRepository.java
@@ -3,7 +3,9 @@ package com.hardik.pomfrey.repository;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.hardik.pomfrey.entity.Request;
@@ -12,4 +14,9 @@ import com.hardik.pomfrey.entity.Request;
 public interface RequestRepository extends JpaRepository<Request, UUID> {
 
 	List<Request> findByFulfilledByUserId(UUID fulfilledByUserId);
+
+	@Query(nativeQuery = true, value = "SELECT *, SQRT(\n" + "    POW(69.1 * (latitude - ?1), 2) +\n"
+			+ "    POW(69.1 * (?2 - longitude) * COS(latitude / 57.3), 2)) AS distance\n"
+			+ "FROM requests ORDER BY distance DESC;")
+	List<Request> findNearestRequests(Double latitude, Double longitude, Pageable pageable);
 }
--- src/main/java/com/hardik/pomfrey/repository/ResourceRepository.java
+++ src/main/java/com/hardik/pomfrey/repository/ResourceRepository.java
@@ -1,13 +1,22 @@
 package com.hardik.pomfrey.repository;
 
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.hardik.pomfrey.entity.Request;
 import com.hardik.pomfrey.entity.Resource;
 
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, UUID> {
+
+	@Query(nativeQuery = true, value = "SELECT *, SQRT(\n" + "    POW(69.1 * (latitude - ?1), 2) +\n"
+			+ "    POW(69.1 * (?2 - longitude) * COS(latitude / 57.3), 2)) AS distance\n"
+			+ "FROM resources ORDER BY distance DESC;")
+	List<Request> findNearestResources(Double latitude, Double longitude, Pageable pageable);
 
 }
